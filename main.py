6038d5ee200db562b213d1409c5459da88c1374c
Mehmet Emre Doğan <87245315+Mehmet-Emre-Dogan@users.noreply.github.com>
2022-10-17 14:20:55 +0300

    finished parsers

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import numpy as np
 import pandas as pd
 
 # https://stackoverflow.com/questions/3518778/how-do-i-read-csv-data-into-a-record-array-in-numpy
-lineData = pd.read_csv('lineData.csv', sep=',', header=None)
-busData = pd.read_csv('busData.csv', sep=',', header=None)
+lineData = pd.read_csv('lineData.csv', sep=',', header=0); lineData.apply(pd.to_numeric, errors='coerce')
+busData = pd.read_csv('busData.csv', sep=',', header=0); busData.apply(pd.to_numeric, errors='coerce')
+print(busData.columns)
+print(busData.loc[:, "voltage"])
