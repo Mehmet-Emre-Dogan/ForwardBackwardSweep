22c0e19ee6aa1ba1a17baaac0013d0e021d33e1e
Mehmet Emre Doğan <87245315+Mehmet-Emre-Dogan@users.noreply.github.com>
2022-11-04 15:45:33 +0300

    add plotting feature

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from constants import *
 from functions import *
 import timeit
+import matplotlib.pyplot  as plt
 DEBUG = False
 
 print("Fetching data...")
@@ -88,3 +89,20 @@ ploss = np.real(np.sum(getPowerArr(iLineArr, lineData.impedance)))
 sOut= getPolar((np.sum(busData.S) + np.sum(getPowerArr(iLineArr, lineData.impedance)) )*S_base*1e3)
 print(f"Line losses: {ploss*S_base*1e3} kW")
 print(f"#S_in = {sIn} kVA  #S_out = {sOut} kVA")
+
+fig, ax = plt.subplots()
+ax.minorticks_on()
+ax2 = ax.twinx()
+# set x-axis label
+ax.set_xlabel("Bus Number", fontsize = 14)
+# set y-axis label
+ax.set_ylabel("v (p.u.)", fontsize=14)
+ax2.set_ylabel("∠°", fontsize=14)
+
+ax.plot(abs(vArr))
+ax2.plot([cmath.phase(v) for v in vArr], color="orange")
+
+ax.grid(color='green',  which='major', linestyle = '--', linewidth = 1)
+ax.grid(color='black',  which='minor', linestyle = '--', linewidth = 0.5)
+ax.set_title('Bus voltages plot')
+plt.show()
